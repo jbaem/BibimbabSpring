687ffeea34ddbdf68c93cbc8e5555650120653a6
jbaem <101052774+jbaem@users.noreply.github.com>
2023-05-21 15:08:26 +0900

    update week2

---
+++ src/main/java/com/springboot/web/PostsApiController.java
@@ -0,0 +1,34 @@
+package com.springboot.web;
+
+import com.springboot.service.posts.PostsService;
+import com.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    @Autowired
+    private final PostsService postsService;
+
+    private final NewService newService;
+
+    @PostMapping("/api/v1/posts")
+    public long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+}
